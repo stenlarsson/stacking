34cecd4ca678f081b3a71e29be4b84d5b8450816
stonebone <sten.larsson@gmail.com>
2010-11-24 19:25:31 +0100

    Implemented drawing

--- Tetatt/Tetatt/XnaHack.cs
+++ Tetatt/Tetatt/XnaHack.cs
@@ -4,15 +4,6 @@ using System;
 namespace Microsoft.Xna.Framework
 {
 	namespace Graphics {
-		public struct Color {
-			public Color(byte r, byte g, byte b, byte a) {
-				R = r; G = g; B = b; A = a;
-			}
-			public readonly byte R, G, B, A;
-			public static Color Black { get { return new Color(0,0,0,255); } }
-			public static Color White { get { return new Color(255,255,255,255); } }
-			public static Color DarkGray { get { return new Color(169,169,169,255); } }
-		}
 		public enum SpriteSortMode { Deferred }
 		public class BlendState {}
 		public class SamplerState {}
