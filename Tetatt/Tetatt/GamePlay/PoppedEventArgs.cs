7cf9a728816d0d6634e490a211a25b16d740bf56
stonebone <sten.larsson@gmail.com>
2010-11-29 19:17:53 +0100

    Implemented remaining sound effects.

---
+++ Tetatt/Tetatt/GamePlay/PoppedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Tetatt.GamePlay
+{
+    class PoppedEventArgs : EventArgs
+    {
+        public Vector2 pos;
+        public bool isGarabge;
+        public Chain chain;
+
+        public PoppedEventArgs(Vector2 pos, bool isGarabge, Chain chain)
+        {
+            this.pos = pos;
+            this.isGarabge = isGarabge;
+            this.chain = chain;
+        }
+    }
+}
