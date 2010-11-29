dc84f5c0ede5f8de4d3e6b757836f6b6d00c7c10
grddev <grddev@gmail.com>
2010-11-30 00:31:07 +0100

    Reimplement SoundEffect using OpenAl to support Pitch changes

--- Tetatt/Tetatt/Xna/SoundEffect.cs
+++ Tetatt/Tetatt/Xna/SoundEffect.cs
@@ -1,48 +1,121 @@
 using System;
 using Tao.Sdl;
+using Tao.OpenAl;
+using System.Collections.Generic;
 
 namespace Microsoft.Xna.Framework.Audio
 {
     public class SoundEffectInstance : IDisposable
     {
-        private SoundEffect effect;
-        public virtual bool IsLooped { get; set; }
-
-        public void Dispose()
-        {
-        }
+        private int source;
 
         internal SoundEffectInstance(SoundEffect effect)
         {
-            this.effect = effect;
+            Al.alGenSources(1, out source);
+            if (Al.alGetError() != Al.AL_NO_ERROR)
+                throw new Exception(Alut.alutGetErrorString(Al.alGetError()));
+
+            Al.alSourcei(source, Al.AL_BUFFER, effect.buffer);
         }
+
+        ~SoundEffectInstance() {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            Al.alDeleteSources(1, ref source);
+            source = Al.AL_NONE;
+        }
+
+        public virtual bool IsLooped {
+            get {
+                int result;
+                Al.alGetSourcei(source, Al.AL_LOOPING, out result);
+                return result == Al.AL_TRUE;
+            }
+            set {
+                Al.alSourcei(source, Al.AL_LOOPING, value ? Al.AL_TRUE : Al.AL_FALSE);
+            }
+        }
+        public float Volume {
+            get {
+                float result;
+                Al.alGetSourcef(source, Al.AL_GAIN, out result);
+                return result;
+            }
+            set {
+                Al.alSourcef(source, Al.AL_GAIN, Math.Max(Math.Min(value,1.0f),0.0f));
+            }
+        }
+        public float Pitch {
+            get {
+                float result;
+                Al.alGetSourcef(source, Al.AL_PITCH, out result);
+                return (result < 1) ? (result-1)*2 : result-1;
+            }
+            set {
+                Al.alSourcef(source, Al.AL_PITCH, value < 0 ? 1+value/2 : 1+value);
+            }
+        }
+        public float Pan {
+            get {
+                return 0.0f;
+            }
+            set {
+                if (value != 0.0f) {
+                    throw new NotImplementedException();
+                }
+            }
+       }
 
         public void Play()
         {
-            if (SdlMixer.Mix_PlayChannel(-1, effect.chunk, IsLooped ? -1 : 0) < 0)
-            {
-                Console.WriteLine("Failed playing sound effect");
-            }
+            Al.alSourcePlay(source);
+            if (Al.alGetError() != Al.AL_NO_ERROR)
+                throw new Exception(Alut.alutGetErrorString(Al.alGetError()));
         }
     }
 
     public sealed class SoundEffect : IDisposable
     {
-        internal IntPtr chunk;
+        internal int buffer;
+
+        private List<SoundEffectInstance> instances = new List<SoundEffectInstance>();
 
         internal SoundEffect (string file)
         {
-            chunk = SdlMixer.Mix_LoadWAV(file);
+            buffer = Alut.alutCreateBufferFromFile(file);
+            if (buffer == Al.AL_NONE) {
+                throw new Exception(Alut.alutGetErrorString(Alut.alutGetError()));
+            }
         }
 
         public void Dispose()
         {
-            SdlMixer.Mix_FreeChunk(chunk);
+            foreach (SoundEffectInstance i in instances) {
+                i.Dispose();
+            }
+            instances.Clear();
+            Al.alDeleteBuffers(1, ref buffer);
         }
 
         public void Play()
         {
-            CreateInstance().Play();
+            Play(1.0f, 1.0f, 0.0f);
+        }
+
+        public void Play(float volume, float pitch, float pan)
+        {
+            // TODO: Remove/Dispose all finished instances...
+            // TODO: Reuse source instead of freeing and reallocating
+            SoundEffectInstance i = new SoundEffectInstance(this){
+                Volume = volume,
+                Pitch = pitch,
+                Pan = pan
+            };
+            instances.Add(i);
+            i.Play();
         }
 
         public SoundEffectInstance CreateInstance()
