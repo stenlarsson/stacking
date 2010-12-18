b718e1be24da618855c3e4dacd5d6f7e0145995d
grddev <grddev@localhost>
2010-12-18 13:45:10 +0100

    Fix compile under mono

--- Tetatt/Tetatt/Xna/MathHelper.cs
+++ Tetatt/Tetatt/Xna/MathHelper.cs
@@ -3,6 +3,8 @@ namespace Microsoft.Xna.Framework
 {
     public static class MathHelper
     {
+        public const float Pi = (float)Math.PI;
+
         public static float Clamp(float val, float min, float max) {
             if (val <= min) {
                 return min;
