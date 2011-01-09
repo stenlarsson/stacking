0ed352bdeebb70feaa039040feed8ab09d9a9037
grddev <grddev@localhost>
2011-01-09 17:13:04 +0100

    Reorganized Xna/Input

---
+++ Tetatt/Tetatt/Xna/Input/GamePad.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Microsoft.Xna.Framework.Input
+{
+    public class GamePad
+    {
+        public static GamePadState GetState(PlayerIndex idex)
+        {
+            return new GamePadState();
+        }
+    }
+}
---
+++ Tetatt/Tetatt/Xna/Input/GamePadButtons.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Microsoft.Xna.Framework.Input
+{
+	public struct GamePadButtons
+    {
+		public ButtonState Back { get { return ButtonState.Released; } }
+	}
+}
