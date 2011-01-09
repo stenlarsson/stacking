7fa92762a3a34f6698d32c4a26f020abeb09b062
grddev <grddev@localhost>
2011-01-09 17:51:29 +0100

    Reformat Xna/ContentManager

--- Tetatt/Tetatt/Xna/Content/ContentManager.cs
+++ Tetatt/Tetatt/Xna/Content/ContentManager.cs
@@ -6,28 +6,32 @@ using Microsoft.Xna.Framework.Audio;
 
 namespace Microsoft.Xna.Framework.Content
 {
-	public class ContentManager
-	{
-		private IServiceProvider serviceProvider;
-		public IServiceProvider ServiceProvider { get { return serviceProvider; } }
-		public string RootDirectory { get; set; }
-		
-		
-		public ContentManager(IServiceProvider serviceProvider)
-		{
-			this.serviceProvider = serviceProvider;
-		}
+    public class ContentManager
+    {
+        private IServiceProvider serviceProvider;
+        public IServiceProvider ServiceProvider
+        {
+            get { return serviceProvider; }
+        }
+        public string RootDirectory { get; set; }
 
-		public virtual T Load<T> (string assetName)
-		{
-			if(typeof(T)==typeof(Texture2D))
-			{
-				return (T)(object)new Texture2D(string.Format("../../../TetattContent/{0}.png", assetName));
-			}
-			else if (typeof(T)==typeof(SoundEffect))
-			{
-				return (T)(object)new SoundEffect(string.Format("../../../TetattContent/{0}.wav", assetName));
-			}
+
+        public ContentManager(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public virtual T Load<T>(string assetName)
+        {
+            if (typeof(T) == typeof(Texture2D))
+            {
+                return (T)(object)new Texture2D(string.Format("../../../TetattContent/{0}.png", assetName));
+            }
+
+            else if (typeof(T) == typeof(SoundEffect))
+            {
+                return (T)(object)new SoundEffect(string.Format("../../../TetattContent/{0}.wav", assetName));
+            }
             else if (typeof(T)==typeof(SpriteFont))
             {
                 return (T)(object)new SpriteFont(
@@ -42,8 +46,8 @@ namespace Microsoft.Xna.Framework.Content
                    )
                 );
             }
-			return default(T);
-		}
-	}
+            return default(T);
+        }
+    }
 }
 
