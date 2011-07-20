a6744f31b37219393205a6b5876cd612cf63adb7
grddev <grddev@gmail.com>
2011-07-20 11:20:30 -0600

    Viewport now correctly adjusts to window size, clients can assume 1280x720 in OpenTK-based xna.

--- Tetatt/Tetatt/Xna/Graphics/Viewport.cs
+++ Tetatt/Tetatt/Xna/Graphics/Viewport.cs
@@ -11,18 +11,50 @@ namespace Microsoft.Xna.Framework.Graphics
         private Rectangle bounds;
         public Rectangle Bounds {
             get { return bounds; }
-            set { bounds = value; GL.Viewport(bounds); }
+            set { bounds = value; glViewport(); }
         }
         public int X { get { return bounds.X; } }
         public int Y { get { return bounds.Y; } }
         public int Width { get { return bounds.Width; } }
         public int Height { get { return bounds.Height; } }
 
-        internal Viewport()
+        private PresentationParameters presentationParameters;
+        internal Viewport(PresentationParameters presentationParameters)
         {
+            this.presentationParameters = presentationParameters;
             int[] values = new int[4];
             GL.GetInteger(GetPName.Viewport, values);
-            bounds = new Rectangle(values[0], values[1], values[2], values[3]);
+            this.windowRectangle = new Rectangle(values[0], values[1], values[2], values[3]);
+            this.bounds = presentationParameters.Bounds;
+        }
+
+        private float scale;
+        internal float windowScale { get { return scale; } }
+        private Rectangle window;
+        internal Rectangle windowRectangle {
+            get { return window; }
+            set {
+                // Use the new window rectangle, but shrunken to maintain aspect ratio
+                var pb = presentationParameters.Bounds;
+                scale = Math.Min(
+                     (float)value.Width / pb.Width,
+                     (float)value.Height / pb.Height);
+                window.Width = (int)(scale * pb.Width);
+                window.Height = (int)(scale * pb.Height);
+                window.X = (value.Width - window.Width) / 2;
+                window.Y = (value.Height - window.Height) / 2;
+
+                glViewport();
+            }
+        }
+
+        private void glViewport()
+        {
+            GL.Viewport(
+                (int)(scale * bounds.X + window.X),
+                (int)(scale * bounds.Y + window.Y),
+                (int)(scale * bounds.Width),
+                (int)(scale * bounds.Height));
         }
     }
 }
