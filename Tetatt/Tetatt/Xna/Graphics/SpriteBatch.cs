2ad8202399b5d76ab0e36598f2fce5ff0765b21d
grddev <grddev@localhost>
2011-08-07 14:16:36 -0600

    Xna: rework Sprite{Batch,Font} implementation

--- Tetatt/Tetatt/Xna/Graphics/SpriteBatch.cs
+++ Tetatt/Tetatt/Xna/Graphics/SpriteBatch.cs
@@ -60,44 +60,119 @@ namespace Microsoft.Xna.Framework.Graphics
             GL.PopMatrix();
         }
 
+        void SetupColor(Color color)
+        {
+            GL.Color4(color.R, color.G, color.B, color.A);
+        }
+
+        void SetupTexture(Texture2D texture)
+        {
+            GL.MatrixMode(MatrixMode.Texture);
+            GL.LoadIdentity();
+            texture.glBindTexture(TextureTarget.Texture2D);
+        }
+
+        void SetupTextureSource(Texture2D texture, float x, float y, float width, float height)
+        {
+            SetupTexture(texture);
+            GL.Scale(1.0f / texture.Width, 1.0f / texture.Height, 1);
+            GL.Translate(x, y, 0);
+            GL.Scale(width, height, 1);
+        }
+
+        void SetupTextureSourceRectangle(Texture2D texture, Rectangle src)
+        {
+            SetupTextureSource(texture, src.X, src.Y, src.Width, src.Height);
+        }
+
+        void SetupTextureSourceMaybeRectangle(Texture2D texture, Rectangle? maybeSrc)
+        {
+            if (maybeSrc.HasValue)
+                SetupTextureSourceRectangle(texture, maybeSrc.Value);
+            else
+                SetupTexture(texture);
+        }
+
+        void SetupModelview()
+        {
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+
+        void SetupPositionWithRotation(float x, float y, float rotation, float ox, float oy)
+        {
+            GL.Translate(x, y, 0);
+            GL.Rotate(180 * rotation / Math.PI, 0, 0, 1);
+            GL.Translate(-ox, -oy, 0);
+        }
+
+        static void SetupSpriteEffects(SpriteEffects effects)
+        {
+            // TODO: Correct this once its needed
+            float st = ((effects & SpriteEffects.FlipHorizontally) != 0) ? -1 : 1;
+            float sv = ((effects & SpriteEffects.FlipVertically) != 0) ? -1 : 1;
+            GL.Translate(0.5f*(st - 1), 0.5f*(sv - 1), 0);
+            GL.Scale(st, sv, 1);
+        }
+
+        void FillSourceRectangle(Texture2D texture, Rectangle? sourceRectangle)
+        {
+            if (sourceRectangle.HasValue)
+                FillRectangle(sourceRectangle.Value.Width, sourceRectangle.Value.Height);
+            else
+                FillRectangle(texture.Width, texture.Height);
+        }
+
+
+        void FillRectangle(float w, float h)
+        {
+            // TODO: Use vertex buffer or whatever...
+            GL.Begin(BeginMode.TriangleStrip);
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(0, 0);
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(0, h);
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(w, 0);
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(w, h);
+            GL.End();
+        }
+
         public void Draw(Texture2D texture, Vector2 position, Color color)
         {
-            Draw(texture, position, null, color);
+            SetupColor(color);
+            SetupTexture(texture);
+            SetupModelview();
+            GL.Translate(position.X, position.Y, 0);
+            FillRectangle(texture.Width, texture.Height);
         }
 
         public void Draw(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color)
         {
-            Rectangle src = sourceRectangle.HasValue ? (Rectangle)sourceRectangle : texture.Bounds;
-            Draw(texture, new Rectangle((int)position.X, (int)position.Y, src.Width, src.Height), src, color);
+            SetupColor(color);
+            SetupTextureSourceMaybeRectangle(texture, sourceRectangle);
+            SetupModelview();
+            GL.Translate(position.X, position.Y, 0);
+            FillSourceRectangle(texture, sourceRectangle);
         }
 
-        public void Draw(Texture2D texture, Rectangle destinationRectangle, Color color)
+        public void Draw(Texture2D texture, Rectangle dest, Color color)
         {
-            Draw(texture, destinationRectangle, null, color);
+            SetupColor(color);
+            SetupTexture(texture);
+            SetupModelview();
+            GL.Translate(dest.X, dest.Y, 0);
+            FillRectangle(dest.Width, dest.Height);
         }
 
         public void Draw(Texture2D texture, Rectangle destinationRectangle, Rectangle? sourceRectangle, Color color)
         {
-            GL.Color4(color.R, color.G, color.B, color.A);
-
-            Rectangle dest = destinationRectangle;
-            Rectangle src = sourceRectangle.HasValue ? (Rectangle)sourceRectangle : texture.Bounds;
-
-            GL.MatrixMode(MatrixMode.Texture);
-            GL.LoadIdentity();
-            GL.Scale(1.0f / texture.Width, 1.0f / texture.Height, 1);
-
-            texture.glBindTexture(TextureTarget.Texture2D);
-            GL.Begin(BeginMode.TriangleStrip);
-            GL.TexCoord2(src.Left, src.Top);
-            GL.Vertex2(dest.Left, dest.Top);
-            GL.TexCoord2(src.Left, src.Bottom);
-            GL.Vertex2(dest.Left, dest.Bottom);
-            GL.TexCoord2(src.Right, src.Top);
-            GL.Vertex2(dest.Right, dest.Top);
-            GL.TexCoord2(src.Right, src.Bottom);
-            GL.Vertex2(dest.Right, dest.Bottom);
-            GL.End();
+            SetupColor(color);
+            SetupTextureSourceMaybeRectangle(texture, sourceRectangle);
+            SetupModelview();
+            GL.Translate(destinationRectangle.X, destinationRectangle.Y, 0);
+            FillRectangle(destinationRectangle.Width, destinationRectangle.Height);
         }
 
         public void Draw(
@@ -111,9 +186,18 @@ namespace Microsoft.Xna.Framework.Graphics
             SpriteEffects effects,
             float layerDepth)
         {
-            // TODO: Implement properly
-            Draw(texture, position, sourceRectangle, color);
+            SetupColor(color);
+            SetupTextureSourceMaybeRectangle(texture, sourceRectangle);
+            SetupSpriteEffects(effects);
+
+            SetupModelview();
+            SetupPositionWithRotation(position.X, position.Y, rotation, origin.X, origin.Y);
+            GL.Scale(scale, scale, 1);
+            GL.Translate(0, 0, layerDepth); // TODO: Could we handle layerDepth like this?
+
+            FillSourceRectangle(texture, sourceRectangle);
         }
+
 
         public void DrawString (SpriteFont spriteFont, string text, Vector2 position, Color color)
         {
@@ -131,29 +215,31 @@ namespace Microsoft.Xna.Framework.Graphics
             SpriteEffects effects,
             float layerDepth)
         {
-            // TODO implement rotation
+            SetupColor(color);
 
-            float x = position.X - scale * origin.X;
-            float y = position.Y - scale * origin.Y;
-            foreach (char c in spriteFont.PrepareString(text))
-            {
-                if (c == '\n')
-                {
-                    x = position.X;
-                    y += spriteFont.LineSpacing * scale;
-                    continue;
-                }
-                Rectangle rect = spriteFont.chars[c];
-                Draw(spriteFont.texture,
-                    new Rectangle(
-                        (int)x,
-                        (int)y,
-                        (int)(rect.Width * scale),
-                        (int)(rect.Height * scale)),
-                    rect,
-                    color);
-                x += (rect.Width + spriteFont.Spacing) * scale;
-            }
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.PushMatrix();
+            // Apply position and rotation to projection so that we can use
+            // the modelview matrix for string-internal positions.
+            SetupPositionWithRotation(position.X, position.Y, rotation, origin.X, origin.Y);
+            GL.Scale(scale, scale, 1);
+
+            // TODO: Handle effects, which probably requires using MeasureString to figure
+            // out the width and height of the string that we are drawing...
+
+            SetupModelview();
+
+            spriteFont._Draw(text, (texture, rect, pos) => {
+                SetupTextureSourceRectangle(texture, rect);
+
+                SetupModelview();
+                GL.Translate(pos.X, pos.Y, 0);
+                FillRectangle(rect.Width, rect.Height);
+            });
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.PopMatrix();
+
         }
     }
 }
