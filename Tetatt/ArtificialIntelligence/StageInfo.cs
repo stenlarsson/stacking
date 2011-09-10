b0918d52454f01d6995e81f9235c80d65494568a
stonebone <sten.larsson@gmail.com>
2011-09-10 13:23:48 +0200

    Add missing StageInfo.cs

---
+++ Tetatt/ArtificialIntelligence/StageInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetatt.ArtificialIntelligence
+{
+    class StageInfo
+    {
+        public readonly int InputDelay;
+        public readonly int RaiseHeight;
+        public readonly int RaiseHeightWithoutGarbage;
+        public readonly float ChainMultiplier;
+
+        public StageInfo(
+            int inputDelay,
+            int raiseHeight,
+            int raiseHeightWithoutGarbage,
+            float chainMultiplier)
+        {
+            InputDelay = inputDelay;
+            RaiseHeight = raiseHeight;
+            RaiseHeightWithoutGarbage = raiseHeightWithoutGarbage;
+            ChainMultiplier = chainMultiplier;
+        }
+    }
+}
